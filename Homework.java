338bcb981edd050de4fc16fd61b562e461c78c11
Mehmet Öztürk <ozturkm2004@gmail.com>
2024-02-07 22:19:53 +0300

    Commentation, Readability Update and Little Code Fix
    Added javadoc for the method.
    Added some space to increase the readability.
    Math.abs was deleted to get negative difference as in the example.

--- Homework.java
+++ Homework.java
@@ -9,17 +9,31 @@ public class Homework {
         System.out.println(findAverage(numbers));
     }
 
-    public static double findAverage (double[] arr){
-        double result = 0;
-        for(double element : arr){
+    /**
+     * This method finds the average of the array and displays how each element
+     * of the array differs from the average. For example, if the array is
+     * {2, 6, 9, 3, 5}, and the average is 5, then the differences from the
+     * average would be {-3, 1, 4, -2, 0}.
+     * 
+     * @param arr the array to work on
+     * @return the average of the array
+     */
+    public static double findAverage (double[] arr) {
+        double result = 0; // The sum of the array
+
+        for (double element : arr){
             result += element;
         }
+
         double average = result / arr.length;
         double [] diff = new double[arr.length];
-        for(int i = 0; i < arr.length; i++){
-            diff[i] = Math.abs(arr[i] - average);
+
+        for (int i = 0; i < arr.length; i++){
+            diff[i] = arr[i] - average;
         }
+
         System.out.println(Arrays.toString(diff));
+
         return average;
     }
     
