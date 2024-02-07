491fab42ac7406de4a511cc184203f856676afc5
Alper Koçyiğit <alper.kocyigit1072@gmail.com>
2024-02-07 10:06:35 +0300

    findaverage

--- Homework.java
+++ Homework.java
@@ -1,1 +1,27 @@
+<<<<<<< Updated upstream
 // This code is awesome!
+=======
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String[] args) {
+        double[] numbers = {2, 6, 9, 3, 5};
+        System.out.println(findAverage(numbers));
+    }
+
+    public static double findAverage (double[] arr){
+        double result = 0;
+        for(double element : arr){
+            result += element;
+        }
+        double average = result / arr.length;
+        double [] diff = new double[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            diff[i] = Math.abs(arr[i] - average);
+        }
+        System.out.println(Arrays.toString(diff));
+        return average;
+    }
+    
+}
+>>>>>>> Stashed changes
